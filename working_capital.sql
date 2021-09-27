c98e5230ad3828fc84af7843921b75bf3562db92
Anne Oluwole <aoluwole7@gmail.com>
2021-09-26 21:49:53 -0500

    Comments added to code

--- working_capital.sql
+++ working_capital.sql
@@ -8,6 +8,7 @@ SELECT Date,
        END Daily_Balance,
        Accounts
 FROM(
+    -- Recursive table expression to accumlate all dates(inclusive) between the first and last transaction dates. 
     WITH dates_cte AS
     (
         SELECT (MIN(DATE::DATE)) Dates, (MAX(DATE::DATE)) End_Date
@@ -21,9 +22,13 @@ FROM(
         tj1.Account Accounts,
         SUM(COALESCE(ar.Credit, 0)) Credit,
         SUM(COALESCE(ar.Debit, 0)) Debit,
+	-- Window function returns the sum of debits minus credits for each account, per day
         SUM(SUM(COALESCE(Debit,0)) - SUM(COALESCE(Credit, 0))) OVER (PARTITION BY tj1.Account ORDER BY dates_cte.Dates) AS Balance
     FROM dates_cte
+    -- Cross join to create a record for each account on every date
     CROSS JOIN (SELECT DISTINCT Account FROM Accounts_Receivable) tj1
+    /*For the dates and accounts present in both queries, the result set will include the dates, 
+    accounts, the sum of debits, and the sum of credits present for which the join conditions are met */ 
     LEFT JOIN Accounts_Receivable ar
         ON ar.Date = dates_cte.Dates
         AND ar.Account = tj1.Account
